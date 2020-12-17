7984ce2f28032eb052ef9ca799b3c13e93167b9c
Ruben <ruben.thomas.nj@gmail.com>
2020-12-17 18:31:41 -0500

    Updates

---
+++ precache-manifest.c21559cfe9005393509c6943c52cc35c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73421cd0a5d91f66072303e471c10363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ceecb9c7f630a3913a",
+    "url": "/static/css/2.f4218568.chunk.css"
+  },
+  {
+    "revision": "968a56d32f27021883f6",
+    "url": "/static/css/main.18d73c1c.chunk.css"
+  },
+  {
+    "revision": "24ceecb9c7f630a3913a",
+    "url": "/static/js/2.f98ebec8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f98ebec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "968a56d32f27021883f6",
+    "url": "/static/js/main.7a554aab.chunk.js"
+  },
+  {
+    "revision": "8cab146f387c6fa6faf4",
+    "url": "/static/js/runtime-main.03d328f6.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "3679a2888b60b459a2f12d957de0e3dd",
+    "url": "/static/media/bg1.3679a288.png"
+  },
+  {
+    "revision": "85982646ca6b5297c8d940d698a1b1ad",
+    "url": "/static/media/bg_frame.85982646.png"
+  },
+  {
+    "revision": "03fbed2e81c2187a84bbb75b1bbeecfc",
+    "url": "/static/media/code_logo.03fbed2e.png"
+  },
+  {
+    "revision": "5ce56666e3afe4cbb5c10b051b284b75",
+    "url": "/static/media/face_round.5ce56666.png"
+  },
+  {
+    "revision": "d22b34cb12a734e66520b8ec6417013d",
+    "url": "/static/media/gm_small.d22b34cb.png"
+  },
+  {
+    "revision": "17e1f05bbc4e4d308b00431318781520",
+    "url": "/static/media/ruben_logo.17e1f05b.png"
+  },
+  {
+    "revision": "7ae3b39f343ffb9eb0ad098be1921a52",
+    "url": "/static/media/rutgers_logo.7ae3b39f.jpg"
+  },
+  {
+    "revision": "d7d07905cf84927f17707b2ebe2e82e6",
+    "url": "/static/media/social_logo.d7d07905.png"
+  }
+]);
